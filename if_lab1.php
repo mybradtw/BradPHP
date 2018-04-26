5bcad05d10f3402af426653db7a8d48cd2cb101d
Brad Chao <brad@brad.tw>
2018-04-26 22:42:59 +0800

    20180426 v2

---
+++ if_lab1.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * if 判斷式練習
+ * Created by PhpStorm.
+ * User: brad
+ * Date: 2018/4/26
+ * Time: 22:35
+ */
+
+    $var1 = true; $var2 = false;
+    if ($var1){
+        echo '$var1 : true' . '<br />';
+    }
+    if (!$var2){    // ! 為非
+        echo '$var2 : false' . '<br />';
+    }
+    echo '<hr />';
+
+    $var1 = 0; $var2 = 0.1;
+    if ($var1) echo 'zero is true' . '<br />';
+    if ($var2) echo 'non-zero is true' . '<br />';
+    echo '<hr />';
+
+    $var1 = ''; $var2 = ' '; $var3 = '0'; $var4 = '00';
+    if ($var1) echo 'non-string is true' . '<br />';
+    if ($var2) echo 'space is true' . '<br />';
+    if ($var3) echo 'zero string is true' . '<br />';
+    if ($var4) echo 'double zero spaces is true' . '<br />';
+    echo '<hr />';
